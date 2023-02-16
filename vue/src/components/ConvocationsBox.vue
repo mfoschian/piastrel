de9ba4d98da8576d4a02993fa122d65a2f23736f
Marco Foschian <marco.foschian@gmail.com>
2023-02-16 23:17:54 +0100

    filtered convocation box

--- vue/src/components/ConvocationsBox.vue
+++ vue/src/components/ConvocationsBox.vue
@@ -5,13 +5,16 @@
 >
 	<div class="title">
 		Convocati: {{ convocations.length || 'ancora nessuno'}}
-		<button v-if="!read_only"
-			class="btn btn-info btn-sm btn-info"
-			title="aggiungi"
-			@click="add_person"
-		>
-			<span class="bi-plus"></span>
-		</button>
+		<div class="inline-bar">
+			<button v-if="!read_only"
+				class="btn btn-info btn-sm btn-info"
+				title="aggiungi"
+				@click="add_person"
+			>
+				<span class="bi-plus"></span>
+			</button>
+			<input type="text" v-model="filter" class="form-control" placeholder="filtra" />
+		</div>
 	</div>
 	<div class="body">
 		<ConvocatedPersonBox  v-for="c in unassigned" :key="c.id"
@@ -24,12 +27,14 @@
 </template>
 
 <script>
-import { watchEffect, computed } from 'vue'
+import { watchEffect, computed, ref } from 'vue'
 
 import ConvocatedPersonBox from '../components/ConvocatedPersonBox.vue'
 import { dropcheck, dropped } from './ConvocatedPersonBox.vue'
 
 import { Convocation } from '../models/Convocation'
+
+import { isEmpty } from '../libs/utils'
 
 export default {
 	components: { ConvocatedPersonBox },
@@ -40,12 +45,25 @@ export default {
 	},
 	setup(props, context) {
 		let _convocations = Convocation.all(); // reactive
+		let filter = ref(null);
 
 		const _unassigned = computed( () => {
 			let cc = _convocations.value || [];
 			// return cc.filter( c => c.bucket_id == null && c.status != Convocation.status.rejected );
-			return cc.filter( c => c.bucket_id == null );
+			let ps = cc.filter( c => c.bucket_id == null );
+			if( !isEmpty(filter.value) && filter.value.length >= 2 ) {
+				let flt = filter.value.toLowerCase();
+				ps = ps.filter( c => c.last_name.toLowerCase().indexOf(flt) >= 0 
+						|| c.first_name.toLowerCase().indexOf(flt) >= 0 );
+			}
+			return ps.sort( (a,b) => {
+				return a.last_name > b.last_name ? 1 :
+					a.last_name < b.last_name ? -1 :
+					a.first_name > b.first_name ? 1 :
+					a.first_name < b.first_name ? -1 : 0;
+			});
 		});
+
 
 		const add_person = async () => {
 			console.log( 'Adding person' );
@@ -68,6 +86,7 @@ export default {
 		return {
 			convocations: _convocations,
 			unassigned: _unassigned,
+			filter,
 			add_person,
 			dropcheck, dropped
 		}
@@ -88,6 +107,20 @@ export default {
 		border-bottom: 1px solid var(--fourth-col);
 		padding-bottom: 0.35rem;
 		padding-left: 0.35rem;
-	}	
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+
+		.inline-bar {
+			display: flex;
+			flex-direction: row;
+			gap: 0.3rem;
+		}
+	}
+
+	.body {
+		max-height: 10rem;
+		overflow-y: auto;
+	}
 }
 </style>
