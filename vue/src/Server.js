071c21594a8d07b0b3287f0fda288233590fe3b1
Marco Foschian <marco.foschian@gmail.com>
2023-03-09 13:26:15 +0100

    bugfix: production using localhost for API

--- vue/src/Server.js
+++ vue/src/Server.js
@@ -1,4 +1,5 @@
-const BASE_URL = 'http://localhost:3000/api'
+// const BASE_URL = 'http://localhost:3000/api'
+const BASE_URL = '/api'
 
 import { getJson, postJson, putJson, deleteJson } from './libs/json-fetch'
 
